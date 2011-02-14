0a00772d3fe018ed83c6a081dec955003b809534
Bobby Johnson <bobby.johnson@gmail.com>
2011-02-14 10:34:07 -0800

    removing chewie directly and adding giles

--- lib/Chewie.0.0.1/chewie-init.ps1
+++
@@ -1,8 +0,0 @@
-function make_a_new_nugetFile
-{
-	new-item -path $pwd -name .NugetFile -itemtype file
-	add-content $pwd\.NugetFile "install_to 'lib'"
-	add-content $pwd\.NugetFile "nuget 'machine.specifications'"
- }
- 
-if(!(test-path $pwd\.NugetFile)){make_a_new_nugetFile}
