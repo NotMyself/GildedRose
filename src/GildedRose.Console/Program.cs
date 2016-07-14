1e97b0de12b22323a0434f2bd76da60026b26c2a
John Doe <jdoe@local>
2016-07-14 17:04:27 -0400

    Refactored inventory loading out of CLI into dedicated class.

--- src/GildedRose.Console/Program.cs
+++ src/GildedRose.Console/Program.cs
@@ -5,7 +5,7 @@ namespace GildedRose.Console
 {
     public class Program
     {
-        public static IList<Item> Items;
+        private static IList<Item> Items;
 
         static void Main(string[] args)
         {
@@ -20,13 +20,13 @@ namespace GildedRose.Console
             System.Console.ReadKey();
         }
 
-        public static void UpdateQuality()
+        private static void UpdateQuality()
         {
             InventoryProcessor inventoryProcessor = new InventoryProcessor();
             inventoryProcessor.UpdateQuality(Items);
         }
 
-        public static void PrintItems(string message, IList<Item> items)
+        private static void PrintItems(string message, IList<Item> items)
         {
             System.Console.WriteLine();
             System.Console.WriteLine(message);
@@ -34,42 +34,10 @@ namespace GildedRose.Console
                 System.Console.WriteLine(item.ToString());
         }
 
-        public static List<Item> LoadInventoryItems()
+        private static List<Item> LoadInventoryItems()
         {
-            List<Item> inventoryItems = new List<Item>()
-            {
-                new InventoryItem {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20, Type = ItemType.Deprecating, QualityRules =
-                {
-                    new QualityRule() { MinSellIn = null, MaxSellIn = -1, Adjustment = QualityAdjustment.Decrease, Rate = 2 },
-                    new QualityRule() { MinSellIn = 0, MaxSellIn = null, Adjustment = QualityAdjustment.Decrease, Rate = 1 }
-                } },
-                new InventoryItem {Name = "Aged Brie", SellIn = 2, Quality = 0, Type = ItemType.Appreciating, QualityRules =
-                {
-                    new QualityRule() { MinSellIn = null, MaxSellIn = -1, Adjustment = QualityAdjustment.Increase, Rate = 2 },
-                    new QualityRule() { MinSellIn = 0, MaxSellIn = null, Adjustment = QualityAdjustment.Increase, Rate = 1 }
-                } },
-                new InventoryItem {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7, Type = ItemType.Deprecating, QualityRules =
-                {
-                    new QualityRule() { MinSellIn = null, MaxSellIn = -1, Adjustment = QualityAdjustment.Decrease, Rate = 2 },
-                    new QualityRule() { MinSellIn = 0, MaxSellIn = null, Adjustment = QualityAdjustment.Decrease, Rate = 1 }
-                } },
-                new InventoryItem {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80, Type = ItemType.Fixed, QualityRules =
-                {
-                    new QualityRule() { MinSellIn = null, MaxSellIn = null, Adjustment = QualityAdjustment.None, Rate = null }
-                } },
-                new InventoryItem { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 15, Quality = 20, Type = ItemType.AppreciatingTiered, QualityRules =
-                {
-                    new QualityRule() { MinSellIn = null, MaxSellIn = -1, Adjustment = QualityAdjustment.SetToMin, Rate = null },
-                    new QualityRule() { MinSellIn = 0, MaxSellIn = 5, Adjustment = QualityAdjustment.Increase, Rate = 3 },
-                    new QualityRule() { MinSellIn = 6, MaxSellIn = 10, Adjustment = QualityAdjustment.Increase, Rate = 2 },
-                    new QualityRule() { MinSellIn = 11, MaxSellIn = null, Adjustment = QualityAdjustment.Increase, Rate = 1 }
-                }},
-                new InventoryItem {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6, Type = ItemType.Deprecating, QualityRules =
-                {
-                    new QualityRule() { MinSellIn = null, MaxSellIn = -1, Adjustment = QualityAdjustment.Decrease, Rate = 4 },
-                    new QualityRule() { MinSellIn = 0, MaxSellIn = null, Adjustment = QualityAdjustment.Decrease, Rate = 2 }
-                }}
-            };
+            InventoryLoader inventoryLoader = new InventoryLoader();
+            List<Item> inventoryItems = inventoryLoader.LoadInventoryItems();
 
             return inventoryItems;
         }
---
+++ src/GildedRose.Inventory.Domain/InventoryLoader.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GildedRose.Inventory.Domain
+{
+    public class InventoryLoader
+    {
+        public List<Item> LoadInventoryItems()
+        {
+            List<Item> inventoryItems = new List<Item>()
+            {
+                new InventoryItem {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20, Type = ItemType.Deprecating, QualityRules =
+                {
+                    new QualityRule() { MinSellIn = null, MaxSellIn = -1, Adjustment = QualityAdjustment.Decrease, Rate = 2 },
+                    new QualityRule() { MinSellIn = 0, MaxSellIn = null, Adjustment = QualityAdjustment.Decrease, Rate = 1 }
+                }},
+                new InventoryItem {Name = "Aged Brie", SellIn = 2, Quality = 0, Type = ItemType.Appreciating, QualityRules =
+                {
+                    new QualityRule() { MinSellIn = null, MaxSellIn = -1, Adjustment = QualityAdjustment.Increase, Rate = 2 },
+                    new QualityRule() { MinSellIn = 0, MaxSellIn = null, Adjustment = QualityAdjustment.Increase, Rate = 1 }
+                }},
+                new InventoryItem {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7, Type = ItemType.Deprecating, QualityRules =
+                {
+                    new QualityRule() { MinSellIn = null, MaxSellIn = -1, Adjustment = QualityAdjustment.Decrease, Rate = 2 },
+                    new QualityRule() { MinSellIn = 0, MaxSellIn = null, Adjustment = QualityAdjustment.Decrease, Rate = 1 }
+                }},
+                new InventoryItem {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80, Type = ItemType.Fixed, QualityRules =
+                {
+                    new QualityRule() { MinSellIn = null, MaxSellIn = null, Adjustment = QualityAdjustment.None, Rate = null }
+                }},
+                new InventoryItem { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 15, Quality = 20, Type = ItemType.AppreciatingTiered, QualityRules =
+                {
+                    new QualityRule() { MinSellIn = null, MaxSellIn = -1, Adjustment = QualityAdjustment.SetToMin, Rate = null },
+                    new QualityRule() { MinSellIn = 0, MaxSellIn = 5, Adjustment = QualityAdjustment.Increase, Rate = 3 },
+                    new QualityRule() { MinSellIn = 6, MaxSellIn = 10, Adjustment = QualityAdjustment.Increase, Rate = 2 },
+                    new QualityRule() { MinSellIn = 11, MaxSellIn = null, Adjustment = QualityAdjustment.Increase, Rate = 1 }
+                }},
+                new InventoryItem {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6, Type = ItemType.Deprecating, QualityRules =
+                {
+                    new QualityRule() { MinSellIn = null, MaxSellIn = -1, Adjustment = QualityAdjustment.Decrease, Rate = 4 },
+                    new QualityRule() { MinSellIn = 0, MaxSellIn = null, Adjustment = QualityAdjustment.Decrease, Rate = 2 }
+                }}
+            };
+
+            return inventoryItems;
+        }
+    }
+}
