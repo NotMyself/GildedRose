2064055b5beb6b74284dbde652734ab9fccdcf0c
rymanb93 <ben.ryman@documation.co.uk>
2017-05-12 14:02:39 +0100

    Add basic test for expected sellIn/quantity after one day for given scenario

--- src/GildedRose.Tests/TestAssemblyTests.cs
+++ src/GildedRose.Tests/TestAssemblyTests.cs
@@ -1,4 +1,6 @@
 using Xunit;
+using GildedRose.Console;
+using System.Collections.Generic;
 
 namespace GildedRose.Tests
 {
@@ -8,6 +10,46 @@ namespace GildedRose.Tests
         public void TestTheTruth()
         {
             Assert.True(true);
+        }
+
+        [Fact]
+        public void UpdateQualityForGivenScenario()
+        {
+            IList<Item> Items = new List<Item>
+                {
+                    new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                    new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+                    new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                    new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                    new Item
+                        {
+                            Name = "Backstage passes to a TAFKAL80ETC concert",
+                            SellIn = 15,
+                            Quality = 20
+                        },
+                    new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+                };
+
+            Items = Program.UpdateQuality(Items);
+
+            Assert.Equal(9, Items[0].SellIn);
+            Assert.Equal(19, Items[0].Quality);
+
+            Assert.Equal(1, Items[1].SellIn);
+            Assert.Equal(1, Items[1].Quality);
+
+            Assert.Equal(4, Items[2].SellIn);
+            Assert.Equal(6, Items[2].Quality);
+
+            Assert.Equal(0, Items[3].SellIn);
+            Assert.Equal(80, Items[3].Quality);
+
+            Assert.Equal(14, Items[4].SellIn);
+            Assert.Equal(21, Items[4].Quality);
+
+            Assert.Equal(2, Items[5].SellIn);
+            Assert.Equal(5, Items[5].Quality);
+
         }
     }
 }
