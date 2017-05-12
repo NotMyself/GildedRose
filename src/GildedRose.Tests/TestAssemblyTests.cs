ec9e6b8b49e7b5cabf6c6dcd9d59d3cc59df50b9
rymanb93 <ben.ryman@documation.co.uk>
2017-05-12 14:42:21 +0100

    Added test for Quality degradation after item sell by

--- src/GildedRose.Tests/TestAssemblyTests.cs
+++ src/GildedRose.Tests/TestAssemblyTests.cs
@@ -51,5 +51,40 @@ namespace GildedRose.Tests
             Assert.Equal(5, Items[5].Quality);
 
         }
+
+        [Fact]
+        public void QualityDegradationAfterSellByDate()
+        {
+            IList<Item> Items = new List<Item>
+            {
+                new Item {Name = "+5 Dexterity Vest", SellIn = 0, Quality = 10 },
+                new Item {Name = "Aged Brie", SellIn = 0, Quality = 10 },
+                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 },
+                new Item
+                        {
+                            Name = "Backstage passes to a TAFKAL80ETC concert",
+                            SellIn = 0,
+                            Quality = 20
+                        }
+            };
+
+            Items = Program.UpdateQuality(Items);
+
+            // Normal item should degrade by 2
+            Assert.Equal(-1, Items[0].SellIn);
+            Assert.Equal(8, Items[0].Quality);
+
+            // Aged Brie should increase by 2
+            Assert.Equal(-1, Items[1].SellIn);
+            Assert.Equal(12, Items[1].Quality);
+
+            // Sulfuras quality and sell by is unchanged
+            Assert.Equal(0, Items[2].SellIn);
+            Assert.Equal(80, Items[2].Quality);
+
+            // Backstage passes quality should be 0
+            Assert.Equal(-1, Items[3].SellIn);
+            Assert.Equal(0, Items[3].Quality);
+        }
     }
 }
