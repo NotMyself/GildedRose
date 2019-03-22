158dd5b1c660c56d1990ec9b22b2ad04ad7ff273
Monica Sima <monica.sima@gmail.com>
2019-03-22 07:30:12 +0200

    Added unit tests for current code to make sure nothing is broken after refactoring. Added unit test for new functionality (Conjured).

--- src/GildedRose.Console/Program.cs
+++ src/GildedRose.Console/Program.cs
@@ -2,39 +2,33 @@
 
 namespace GildedRose.Console
 {
-    class Program
+    internal class Program
     {
-        IList<Item> Items;
+        internal static IList<Item> Items;
         static void Main(string[] args)
         {
             System.Console.WriteLine("OMGHAI!");
 
-            var app = new Program()
-                          {
-                              Items = new List<Item>
-                                          {
-                                              new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
-                                              new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
-                                              new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
-                                              new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
-                                              new Item
-                                                  {
-                                                      Name = "Backstage passes to a TAFKAL80ETC concert",
-                                                      SellIn = 15,
-                                                      Quality = 20
-                                                  },
-                                              new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
-                                          }
-
-                          };
-
-            app.UpdateQuality();
-
+            Items = new List<Item>
+            {
+                new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+                new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 15,
+                    Quality = 20
+                },
+                new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+            };
+            UpdateQuality();
             System.Console.ReadKey();
 
         }
 
-        public void UpdateQuality()
+        internal static void UpdateQuality()
         {
             for (var i = 0; i < Items.Count; i++)
             {
@@ -109,7 +103,6 @@ namespace GildedRose.Console
                 }
             }
         }
-
     }
 
     public class Item
---
+++ src/GildedRose.Tests/ProgramTests.cs
@@ -0,0 +1,154 @@
+using Xunit;
+using GildedRose.Console;
+using System.Collections.Generic;
+
+namespace GildedRose.Tests
+{
+    public class ProgramTests
+    {
+        #region Test Quality
+        [Theory]
+        [InlineData(1, 10, 11)]
+        [InlineData(0, 50, 50)]
+        [InlineData(-1, 0, 2)]
+        [InlineData(-1, 48, 50)]
+        [InlineData(-1, 49, 50)]
+        [InlineData(-1, 50, 50)]
+        //[InlineData(30, int.MaxValue, 50)]
+        //[InlineData(int.MinValue, 2, 4)]
+        public void UpdateQuality_AgedBrieQualityIsCorrect(int sellInActual, int qualityActual, int qualityExpected)
+        {
+            IList<Item> actual = GetUpdatedItems(sellInActual, qualityActual, "Aged Brie");
+
+            Assert.Equal(qualityExpected, actual[0].Quality);
+        }
+
+        [Theory]
+        [InlineData(1, 10, 10)]
+        [InlineData(0, 50, 50)]
+        [InlineData(0, 80, 80)]
+        [InlineData(-1, 0, 0)]
+        //[InlineData(int.MinValue, 1, 1)]
+        public void UpdateQuality_SulfurasQualityIsCorrect(int sellInActual, int qualityActual, int qualityExpected)
+        {
+            IList<Item> actual = GetUpdatedItems(sellInActual, qualityActual, "Sulfuras, Hand of Ragnaros");
+
+            Assert.Equal(qualityExpected, actual[0].Quality);
+        }
+
+        [Theory]
+        [InlineData(6, 10, 12)]
+        [InlineData(5, 10, 13)]
+        [InlineData(10, 10, 12)]
+        [InlineData(11, 10, 11)]
+        [InlineData(0, 51, 0)]
+        [InlineData(-1, 48, 0)]
+        //[InlineData(int.MaxValue, 48, 49)]
+        //[InlineData(int.MinValue, 4, 0)]
+        public void UpdateQuality_BackstagePassesQualityIsCorrect(int sellInActual, int qualityActual, int qualityExpected)
+        {
+            IList<Item> actual = GetUpdatedItems(sellInActual, qualityActual, "Backstage passes to a TAFKAL80ETC concert");
+
+            Assert.Equal(qualityExpected, actual[0].Quality);
+        }
+
+        [Theory]
+        [InlineData(1, 10, 9)]
+        [InlineData(0, 50, 48)]
+        [InlineData(-1, 0, 0)]
+        [InlineData(-1, 1, 0)]
+       // [InlineData(int.MinValue, 4, 2)]
+        public void UpdateQuality_RegularItemQualityIsCorrect(int sellInActual, int qualityActual, int qualityExpected)
+        {
+            IList<Item> actual = GetUpdatedItems(sellInActual, qualityActual, "Elixir of the Mongoose");
+
+            Assert.Equal(qualityExpected, actual[0].Quality);
+        }
+
+        [Theory]
+        [InlineData(1, 10, 8)]
+        [InlineData(0, 10, 6)]
+        [InlineData(1, 1, 0)]
+        [InlineData(-1, 3, 0)]
+        [InlineData(-1, 0, 0)]
+        //[InlineData(int.MinValue, 3, 0)]
+        public void UpdateQuality_ConjuredQualityIsCorrect(int sellInActual, int qualityActual, int qualityExpected)
+        {
+            IList<Item> actual = GetUpdatedItems(sellInActual, qualityActual, "Conjured Mana Cake");
+
+            Assert.Equal(qualityExpected, actual[0].Quality);
+        }
+        #endregion
+
+        #region Test SellIn
+        [Theory]
+        [InlineData(1, 10, 1)]
+        [InlineData(-1, 0, -1)]
+        [InlineData(0, 48, 0)]
+        //[InlineData(int.MinValue, 80, int.MinValue)]
+        public void UpdateQuality_SulfurasSellInIsCorrect(int sellInActual, int qualityActual, int sellInExpected)
+        {
+            IList<Item> actual = GetUpdatedItems(sellInActual, qualityActual, "Sulfuras, Hand of Ragnaros");
+
+            Assert.Equal(sellInExpected, actual[0].SellIn);
+        }
+
+        [Theory]
+        [InlineData(1, 10, 0)]
+        [InlineData(0, 10, -1)]
+        [InlineData(-1, 0, -2)]
+        public void UpdateQuality_AgedBrieSellInIsCorrect(int sellInActual, int qualityActual, int sellInExpected)
+        {
+            IList<Item> actual = GetUpdatedItems(sellInActual, qualityActual, "Aged Brie");
+
+            Assert.Equal(sellInExpected, actual[0].SellIn);
+        }
+
+        [Theory]
+        [InlineData(1, 10, 0)]
+        [InlineData(0, 10, -1)]
+        [InlineData(-1, 0, -2)]
+        public void UpdateQuality_BackstagePassesSellInIsCorrect(int sellInActual, int qualityActual, int sellInExpected)
+        {
+            IList<Item> actual = GetUpdatedItems(sellInActual, qualityActual, "Backstage passes to a TAFKAL80ETC concert");
+
+            Assert.Equal(sellInExpected, actual[0].SellIn);
+        }
+
+        [Theory]
+        [InlineData(1, 10, 0)]
+        [InlineData(0, 10, -1)]
+        [InlineData(-1, 0, -2)]
+        public void UpdateQuality_RegularItemSellInIsCorrect(int sellInActual, int qualityActual, int sellInExpected)
+        {
+            IList<Item> actual = GetUpdatedItems(sellInActual, qualityActual, "Elixir of the Mongoose");
+
+            Assert.Equal(sellInExpected, actual[0].SellIn);
+        }
+
+        [Theory]
+        [InlineData(1, 10, 0)]
+        [InlineData(0, 10, -1)]
+        [InlineData(-1, 0, -2)]
+        public void UpdateQuality_ConjuredSellInIsCorrect(int sellInActual, int qualityActual, int sellInExpected)
+        {
+            IList<Item> actual = GetUpdatedItems(sellInActual, qualityActual, "Conjured Mana Cake");
+
+            Assert.Equal(sellInExpected, actual[0].SellIn);
+        }
+        #endregion
+
+        private static IList<Item> GetUpdatedItems(int sellInActual, int qualityActual, string itemName)
+        {
+            Program.Items = new List<Item>
+            {
+                new Item {Name = itemName , SellIn = sellInActual, Quality = qualityActual}
+            };
+
+            Program.UpdateQuality();
+
+            return Program.Items;
+        }
+
+    }
+}
