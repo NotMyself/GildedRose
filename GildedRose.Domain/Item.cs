d8ce39c8c413ed89e68c57ab507f8c2b3e9bf173
Tim Collins <tim@timcollins.ie>
2019-12-13 17:50:20 +0000

    Added Domain project and moved Item class to it.

---
+++ GildedRose.Domain/Item.cs
@@ -0,0 +1,9 @@
+﻿namespace GildedRose.Domain
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public int SellIn { get; set; }
+        public int Quality { get; set; }
+    }
+}
