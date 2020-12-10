55a5daaa4809530f25d4d794d156ae17762988b3
ATG\nbeaird <nbeaird@hagerty.com>
2020-12-09 16:59:41 -0800

    Add  tests outlining core project expectations

--- src/GildedRose.Tests/TestAssemblyTests.cs
+++ src/GildedRose.Tests/TestAssemblyTests.cs
@@ -1,13 +1,56 @@
 using Xunit;
+using GildedRose.Console;
 
 namespace GildedRose.Tests
 {
     public class TestAssemblyTests
     {
         [Fact]
-        public void TestTheTruth()
+        public void TestObjectName()
         {
-            Assert.True(true);
+            Item item = new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20 };
+            Assert.Equal(item.Name, "+5 Dexterity Vest");
+        }
+        [Fact]
+        public void Item_WithinSellinTimePeriod_QualityDegradesByOnePointPerDay()
+        {
+
+        }
+        [Fact]
+        public void Item_PastSellinTimePeriod_QualityDegradesByTwoPoints()
+        {
+
+        }
+        [Fact]
+        public void Item_PastQualityDegradationMax_QualityIsNeverNegative()
+        {
+
+        }
+        [Fact]
+        public void Item_NamedBrie_QualityIncreasesWithAge()
+        {
+
+        }
+        [Fact]
+        public void Item_NamedSulfuras_NeverHasToBeSoldOrDecreasesValue()
+        {
+
+        }
+        [Fact]
+        public void Item_NamedSulfuras_HasValueOf80()
+        {
+
+        }
+        [Fact]
+        public void Item_QualityIncreases_QualityNeverExceedsMaxValue()
+        {
+
+        }
+        [Fact]
+        public void Item_BackStagePass_IncreaseInValueTowardsSellin()
+        {
+
         }
     }
+
 }
